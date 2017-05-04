73e9733cc4d1997138d292f2b9be494760d199b5
robzdoom <jorgecruz.rd@gmail.com>
2017-05-04 11:27:34 -0700

    added Bootstrap and Header component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './header.js'
 import styles from './app.css';
 
 const App = () => (
     <div className={styles.app}>
         <h1>Welcome to React!</h1>
+        <Header />
     </div>
 );
 
