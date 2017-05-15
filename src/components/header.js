d06050105926064dc9a89d92f5613931b533a88c
robzdoom <jorgecruz.rd@gmail.com>
2017-05-15 09:41:39 -0700

    added grade average

--- src/components/header.js
+++ src/components/header.js
@@ -5,52 +5,54 @@ export default class Header extends Component {
   constructor(){
     super()
       this.state = {
-        grade: 0,
+        grade: null,
         listOfStudents: []
       }
   }
- // ----------------------Making FireBase Call --------------------------------
- componentDidMount (){
-    database.ref('students').on('value', snapshot =>{
-            this.setState({
-              listOfStudents: snapshot.val()
-            })
-        })
-    }
-  renderGradeAverage (){
-    const {listOfStudents} = this.state;
-    
-    if(!listOfStudents){
-            return 
+
+// ----------------------Making FireBase Call --------------------------------
+  componentDidMount() {
+    const studentsRef = database.ref('students');
+    studentsRef.on('value', snap => {
+        // get value of the firebase data
+        const students = snap.val();
+        const gradeArray = []
+        // To iterate through object data received from firebase, goes through the keys of the object to map through
+        if(!students){
+          this.setState({
+            gradeAverage: null
+          })
+          return
         }
-    return Object.keys(listOfStudents).map(key => {
-            const {grade} = listOfStudents[key]
-            
-          const numbers =  parseInt(grade, 10);
-
-
-          //  const total = 0;
-          //  const allNumbers = total + numbers;
-
-           console.log(allNumbers)
-
-            return (
-              <h6 key={key}>{grade}</h6>
-            )
-
+//-----------------------------Made object.key call to get the average of students. ------------------------------
+        Object.keys(students).map(key => {
+          const grade = parseInt(students[key].grade, 10);
+          gradeArray.push(grade);
+          return gradeArray
+        });
+        const average = gradeArray.reduce((total, amount, index, array) => {
+          total += amount;
+          if( index === array.length-1){
+            return total/array.length;
+          }else {
+            return total;
+          }
         })
-            
+        this.setState({
+          gradeAverage: Math.round(average)
+        })
+      })
   }
 
   render(){
     return(
       <div className="page-header">
         <h1 className="hidden-xs hidden-sm">Student Grade Table
-          <small className="pull-right">Grade Average: <span className="label label-default"></span>{this.renderGradeAverage()}</small>
+          <small className="pull-right">Grade Average: <span className="label label-default"></span>{this.state.gradeAverage}</small>
         </h1>
         <h3 className="hidden-md hidden-lg">Student Grade Table 
           <small className="pull-right">Grade Average: 
-          <span className="label label-default"></span></small>
+          <span className="label label-default">{this.state.gradeAverage}</span></small>
           
         </h3>
         
